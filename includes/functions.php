7bd62316bd9512d0d4f6a97d4e0c11b0f2673441
Venugopalkrishna <s_venugopalkrishna@tss-hr420-dm21.local>
2017-12-04 17:44:59 -0500

    Ajax

---
+++ includes/functions.php
@@ -0,0 +1,42 @@
+<?php
+  $user = "root";
+  $pass = "root"; // this should be a blank screen for windows machines
+  $host = "localhost";
+  $db = "db_cooper"; //whatever you called your database
+
+  $conn = mysqli_connect($host, $user, $pass, $db);
+  mysqli_set_charset($conn, 'utf8');
+
+  if (!$conn) {
+    echo "something broke";
+    exit;
+  }
+
+  // echo "connectedd";
+
+  $myQuery = "SELECT * FROM mainmodel";
+  $result = mysqli_query($conn, $myQuery);
+
+  $rows = array();
+
+  while($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  // var_dump($rows);
+  // echo json_encode($rows);
+
+
+  // get a single row (one result) using a query paramete
+  if (isset($_GET['carModel'])) { //see if there's a parameter called carModel
+    $car = $_GET['carModel']; //whatever comes adter the ?
+
+    $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'";
+    $result = mysqli_query($conn, $myQuery);
+
+    $row =mysqli_fetch_assoc($result);
+
+    echo json_encode($row);
+  }
+
+?>
--- js/main.js
+++ js/main.js
@@ -1,34 +1,62 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
+
 var F55 = document.querySelector("#F55"),
     F56 = document.querySelector("#F56"),
     R58 = document.querySelector("#R58"),
-    modelName = document.querySelector(".modelName"),
+    model = document.querySelector(".modelName"),
     priceInfo = document.querySelector(".priceInfo"),
-    modelDetails = document.querySelector(".modelDetails"),
+    details = document.querySelector(".modelDetails"),
     theImages = document.querySelectorAll('.data-ref');
+const httpRequest = new XMLHttpRequest();
 
 
-function changeDetails(){
-  let objectIndex = carData[this.id];
-  console.log("works");
-for(var i=0; i<theImages.length; i++)
+    function getCarData(){
+if(!httpRequest)
 {
-  theImages[i].classList.add("nonActive");
+  alert("Browser old");
+  return false;
 }
-this.classList.remove("nonActive");
 
-  modelName.firstChild.nodeValue = objectIndex.headline;
-  priceInfo.firstChild.nodeValue = objectIndex.price;
-  modelDetails.firstChild.nodeValue = objectIndex.text;
+httpRequest.onreadystatechange = processRequest;
+httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
+httpRequest.send();
+    }
 
+    function processRequest() {
+      let reqIndicator = document.querySelector('.request-state');
+      reqIndicator.textContent = httpRequest.readyState;
+      debugger;
+
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) { // 200 means everything is awesome
+          let data = JSON.parse(httpRequest.responseText);
+
+          processResult(data);
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+
+function processResult(data){
+const { modelName, pricing, modelDetails} = data;
+
+model.textContent = modelName;
+priceInfo.innerHTML = pricing;
+details.textContent = modelDetails;
+
+theImages.forEach(function(image, index){
+  image.classList.add('nonActive');
+});
+document.querySelector(`#${data.model}`).classList.remove('nonActive');
 
 }
 
 theImages.forEach(function(image, index){
-        image.addEventListener('click', changeDetails, false);
+        image.addEventListener('click', getCarData, false);
       });
 
-changeDetails.call(document.querySelector("#F55"));
+getCarData.call(document.querySelector("#F55"));
 
 })();
