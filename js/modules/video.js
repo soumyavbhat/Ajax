8237e427c6eec04cc7078bb08f09df878401ba7a
Venugopalkrishna <s_venugopalkrishna@tss-hr420-dm21.fc.ca>
2017-12-11 17:09:37 -0500

    videos added, etc etc

---
+++ js/modules/video.js
@@ -0,0 +1,46 @@
+var video = {
+  videoPlayer : document.querySelector('video'),
+  vidThumbs : document.querySelector('.vid_thumb'),
+  volumeIndicator : document.querySelector('.vol-indicator'),
+
+volOn(){
+
+  video.videoPlayer.muted = false;
+  video.volumeIndicator.classList.replace('fa-volume-off', 'fa-volume-up');
+  console.log("on");
+
+},
+
+volOff(){
+
+  video.videoPlayer.muted = true;
+  video.volumeIndicator.classList.replace('fa-volume-up', 'fa-volume-off');
+
+  console.log("off");
+
+},
+
+popOverlay(){
+  let overlay = document.querySelector('.vid-overlay');
+  overlay.classList.add('show-overlay');
+  overlay.querySelector('i').addEventListener('click', video.replayVideo, false);
+},
+
+replayVideo(){
+  video.videoPlayer.currentTime = 0;
+  video.videoPlayer.play();
+
+  let overlay = document.querySelector('.vid-overlay');
+  overlay.classList.remove('show-overlay');
+},
+
+  init(){
+  console.log("video module added");
+  video.videoPlayer.addEventListener('mouseover', video.volOn, false);
+  video.videoPlayer.addEventListener('mouseout', video.volOff, false);
+  video.videoPlayer.addEventListener('ended', video.popOverlay, false);
+
+}
+}
+
+video.init();
