07e962303dfc4db5fa41acfc83df4f19600de5d8
Venugopalkrishna <s_venugopalkrishna@tss-hr420-dm21.fc.ca>
2017-12-11 16:14:37 -0500

    shorthand php

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@
 var F55 = document.querySelector("#F55"),
     F56 = document.querySelector("#F56"),
     R58 = document.querySelector("#R58"),
-    model = document.querySelector(".modelName"),
-    priceInfo = document.querySelector(".priceInfo"),
-    details = document.querySelector(".modelDetails"),
+
     theImages = document.querySelectorAll('.data-ref');
 const httpRequest = new XMLHttpRequest();
 
@@ -17,7 +15,16 @@ const httpRequest = new XMLHttpRequest();
       // the fetch api uses new javascript promise api
       fetch(url)
       .then((resp)=>resp.json())
-      .then((data)=>{processResult(data);})
+      .then(({modelName, pricing, modelDetails, model})=>{
+        let carModel = document.querySelector(".modelName").textContent = modelName;
+        let priceInfo = document.querySelector(".priceInfo").innerHTML = pricing;
+        let details = document.querySelector(".modelDetails").textContent = modelDetails;
+
+        theImages.forEach(function(image, index){
+          image.classList.add('nonActive');
+      });
+      document.querySelector(`#${model}`).classList.remove('nonActive');
+    })
       .catch(function(error)
       {
         console.log(error);
@@ -34,40 +41,40 @@ const httpRequest = new XMLHttpRequest();
 // httpRequest.send();
     }
 
-    function processRequest() {
-      let reqIndicator = document.querySelector('.request-state');
-      reqIndicator.textContent = httpRequest.readyState;
-      // debugger;
+    // function processRequest() {
+    //   let reqIndicator = document.querySelector('.request-state');
+    //   reqIndicator.textContent = httpRequest.readyState;
+    //   // debugger;
+    //
+    //   if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    //     if (httpRequest.status === 200) { // 200 means everything is awesome
+    //       let data = JSON.parse(httpRequest.responseText);
+    //
+    //       processResult(data);
+    //     } else {
+    //       alert('There was a problem with the request.');
+    //     }
+    //   }
+    // }
 
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) { // 200 means everything is awesome
-          let data = JSON.parse(httpRequest.responseText);
-
-          processResult(data);
-        } else {
-          alert('There was a problem with the request.');
-        }
-      }
-    }
-
-function processResult(data){
-const { modelName, pricing, modelDetails} = data;
-
-model.textContent = modelName;
-priceInfo.innerHTML = pricing;
-details.textContent = modelDetails;
-
-theImages.forEach(function(image, index){
-  image.classList.add('nonActive');
-});
-document.querySelector(`#${data.model}`).classList.remove('nonActive');
-
-}
+// function processResult(data){
+// const { modelName, pricing, modelDetails} = data;
+//
+// // model.textContent = modelName;
+// // priceInfo.innerHTML = pricing;
+// // details.textContent = modelDetails;
+// //
+// // theImages.forEach(function(image, index){
+// //   image.classList.add('nonActive');
+// // });
+// // document.querySelector(`#${data.model}`).classList.remove('nonActive');
+// //
+// }
 
 theImages.forEach(function(image, index){
         image.addEventListener('click', getCarData, false);
       });
 
-getCarData.call(document.querySelector("#F55"));
+// getCarData.call(document.querySelector("#F55"));
 
 })();
